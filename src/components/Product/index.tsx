8f13ed353641619899b808f78bb4e865bdcad996
Carolina Mattos <mattos.carolina98@gmail.com>
2024-01-04 23:52:30 -0300

    adicionando modal

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -6,13 +6,30 @@ type Props = {
   description: string
 }
 const Product = ({ title, image, description }: Props) => (
-  <S.CardProduct>
-    <img src={image} alt="pizza" />
-    <h2>{title}</h2>
-    <p>{description}</p>
-    <Button type="button" title="Clique aqui para adicionar ao carrinho">
-      Adicionar ao carrinho
-    </Button>
-  </S.CardProduct>
+  <>
+    <S.CardProduct>
+      <img src={image} alt="pizza" />
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <Button type="button" title="Clique aqui para adicionar ao carrinho">
+        Adicionar ao carrinho
+      </Button>
+    </S.CardProduct>
+
+    <S.Modal>
+      <S.ModalContent className="container">
+        <img src={image} alt="pizza" />
+        <div>
+          <h2>{title}</h2>
+          <p>{description}</p>
+          <p>Serve: de 2 a 3 pessoas</p>
+          <Button type="button" title="Clique aqui para adicionar ao carrinho">
+            Adicionar ao carrinho - R$60,90
+          </Button>
+        </div>
+      </S.ModalContent>
+      <div className="overlay"></div>
+    </S.Modal>
+  </>
 )
 export default Product
--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -13,3 +13,28 @@ export const CardProduct = styled.div`
     font-size: 16px;
   }
 `
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    // background-color: rgba(0, 0, 0, 0.73);
+  }
+`
+export const ModalContent = styled.div`
+  background-color: ${palette.orange};
+  posisition: relative;
+  z-index: 1;
+`
